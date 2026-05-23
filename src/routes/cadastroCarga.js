b3a37191b8b2cb34d915012b9734d843e0c3d68a
EduardoGuaglioniLupianez <eduardo.lupianez@sptech.school>
2026-05-23 00:01:47 -0300

    comentando cadastro carga rotas

--- src/routes/cadastroCarga.js
+++ src/routes/cadastroCarga.js
@@ -1,15 +1,15 @@
 var express = require("express");
 var router = express.Router();
 var cadastroCargaController = require("../controllers/cadastroCargaController");
-
+// cadatrar carga rota
 router.post("/cadastrarCarga", function(req, res) {
     cadastroCargaController.cadastrarCarga(req, res);
 });
-
+// buscar lotes 
 router.get("/lotes", function(req, res) {
     cadastroCargaController.listarLotes(req, res);
 });
-
+// buscar sensores
 router.get("/sensores", function(req, res) {
     cadastroCargaController.listarSensoresDisponiveis(req, res);
 });
