d8c4180ede805496adb029e746c82c527ceaf8cb
nicky-rodrigues <nicole.nascimento@sptech.school>
2026-06-03 14:52:57 -0300

    Alterando função atualizar do model de status de carga para desvincular sensor após a carga ter sido entregue

--- src/models/statusCargaModel.js
+++ src/models/statusCargaModel.js
@@ -9,15 +9,36 @@ function atualizar(codigoCarga, novaLocalizacao, statusCarga) {
         statusCarga
     );
 
-    var instrucaoSql = `
-        UPDATE entrega e
-        JOIN carga c 
-            ON e.fk_codigo_carga = c.codigo_Carga
-        SET 
-            e.ultima_loc = '${novaLocalizacao}',
-            c.status_carga = '${statusCarga}'
-        WHERE c.codigo_Carga = '${codigoCarga}';
-    `;
+    var instrucaoSql = "";
+
+    if (statusCarga == "Entregue") {
+        instrucaoSql = `
+            UPDATE entrega e
+            JOIN carga c 
+                ON e.fk_codigo_carga = c.codigo_Carga
+            LEFT JOIN monitoramento_sensor ms
+                ON ms.fk_carga = c.id_carga
+                AND ms.dt_fim IS NULL
+            LEFT JOIN sensor s
+                ON s.id_sensor = ms.fk_sensor
+            SET 
+                e.ultima_loc = '${novaLocalizacao}',
+                c.status_carga = '${statusCarga}',
+                ms.dt_fim = NOW(),
+                s.status_sensor = 'Disponível'
+            WHERE c.codigo_Carga = '${codigoCarga}';
+        `;
+    } else {
+        instrucaoSql = `
+            UPDATE entrega e
+            JOIN carga c 
+                ON e.fk_codigo_carga = c.codigo_Carga
+            SET 
+                e.ultima_loc = '${novaLocalizacao}',
+                c.status_carga = '${statusCarga}'
+            WHERE c.codigo_Carga = '${codigoCarga}';
+        `;
+    }
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
 
